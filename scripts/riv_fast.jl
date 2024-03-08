dd4ac1b7361d1e817ca483fb81894f015f6a4a0c
Zitzeronion <physiknerd@gmail.com>
2024-03-08 16:00:22 +0100

    Newer growth plot

--- scripts/riv_fast.jl
+++ scripts/riv_fast.jl
@@ -60,7 +60,10 @@ end
 
 # ╔═╡ a3190736-9b19-4355-9843-33215b1707de
 begin
-	growths = plot(xlabel = L"t/\tau_m", ylabel = L"\Delta h / h_d",
+	times_now = zeros(101)
+	tscale = []
+	finalH = []
+	growths = plot(xlabel = L"t/10^5\tau_m", ylabel = L"\Delta h / h_d",
 		# xaxis=:log10,
 		yaxis=:log10,
 		grid = false,
@@ -70,34 +73,43 @@ begin
 		minorticks = true,
 		legend = :bottomright,
 		# w = 2,
-		# ylims = (0.003, 1.1),
-		xlims = (0.0, 3.5)
+		ylims = (0.003, 1.01),
+		xlims = (0.0, 26)
 	)
-	inconds = [(150, 20, 40, 2π/9, :ut), (180, 20, 40, 2π/9, :circ), (200, 40, 20, π/9, :hex), (150, 20, 30, π/6, :dt), (180, 20, 30, π/6, :rect), (200, 20, 30, π/6, :diamond), (150, 20, 20, π/9, :xcross), (180, 20, 20, π/9, :star5),  (200, 20, 20, π/9, :cross),   ]
+	inconds = [(150, 20, 40, 2π/9, :ut), (180, 20, 40, 2π/9, :circ), (150, 20, 30, π/6, :hex), (180, 20, 30, π/6, :dt), (200, 20, 30, π/6, :rect), (150, 20, 20, π/9, :diamond), (180, 20, 20, π/9, :star5),  (200, 20, 20, π/9, :cross),   ]
 	for ics in inconds
 		iC = initial_data[(initial_data.R0 .== ics[1]) .& (initial_data.rr0 .== ics[2]) .& (initial_data.angle .== ics[3]), :]
 
 		LL = iC.realrr[1]# sqrt(iC.realrr[1] * iC.rr0[1]) #sqrt(iC.R0[1]*iC.maxh0[1])
 		om = ((0.01*0.0379*ics[4]^3/(sin(ics[4]))*sqrt(ics[4] - sin(2ics[4])/2)*1/LL))
-		tt = 1/om
+		tt = 10000*3/2(iC.R0[1] + iC.realrr[1] - iC.rdrop[1])/(0.01 * ics[4])
 		
 		tnorm = iC.tauMax[1] #(0.5 * 2iC.realrr / 0.01)
 	 	dataSamp = growthDF[(growthDF.R0 .== ics[1]) .& (growthDF.rr0 .== ics[2]) .& (growthDF.theta .== ics[3]) .& (growthDF.substrate .== "uniform"), :]
-		scatter!(dataSamp.time[2:end] ./ iC.tauMax[1], 
+		scatter!(dataSamp.time[2:end] ./ tt, # iC.tauMax[1], 
 			dataSamp.deltaH[2:end] ./ iC.hdrop[1], 
 		# label=latexstring("\$(\\psi_0, \\theta) = ({$(round(iC.psi0[1], digits=3))}, {$(ics[3])})\$"),
 		label="(θ, ψ₀) = ($(ics[3])°, $(round(iC.psi0[1], digits=3)))",
 		m = (8, ics[5], 0.75)
 		)
-	
-		# plot!(dataSamp.time[1:end] ./ iC.tauMax[1], (0.16 .* exp.(iC.sigmaMax[1] .* dataSamp.time[1:end]) .+ 0.04) ./ iC.hdrop[1], label="Exponential fit", l = (2,  :dashdot, :black))
+		if ics[5] == :hex
+			push!(tscale, tt)
+			push!(finalH, iC.hdrop[1])
+			times_now .= dataSamp.time
+			# println("length: $(length(dataSamp.time))")
+		end
 	end
+	# println(tscale[1], finalH[1])
+	plot!(times_now ./ tscale[1], (0.01 .* exp.(0.39 .* times_now ./ tscale[1]) .+ 0.04) ./ finalH[1], label="Exponential fit", l = (2,  :dash, :black))
 	#plot!(subdata2.time[1:end] ./ T0, (0.001 .* exp.(0.000008 .* subdata2.time[1:end]) .+ 0.14) ./ H0, 
 		#label="",
 		# l = (2,  :dashdot, :black))
 
 	growths
 end
+
+# ╔═╡ dd19e06e-999b-4a92-afc3-4020304f7bb2
+times_now
 
 # ╔═╡ b92731a3-b78f-4bc4-a40c-8f14d49a2ff5
 savefig(growths, "../assets/growth-breakup.pdf")
@@ -2106,6 +2118,7 @@ version = "1.4.1+1"
 # ╠═b43862d9-24c9-4a25-ab45-c9e016331cae
 # ╠═3544b7df-56be-4fe2-aa45-753cd04a5439
 # ╠═a3190736-9b19-4355-9843-33215b1707de
+# ╠═dd19e06e-999b-4a92-afc3-4020304f7bb2
 # ╠═b92731a3-b78f-4bc4-a40c-8f14d49a2ff5
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
