b1250b60b4fc5cdf124f7c3a24f07d40df8b9544
Zitzeronion <physiknerd@gmail.com>
2024-03-11 13:41:56 +0100

    Okay first plot

--- scripts/riv_fast.jl
+++ scripts/riv_fast.jl
@@ -67,22 +67,20 @@ begin
 		# xaxis=:log10,
 		yaxis=:log10,
 		grid = false,
-		legendfontsize = 11,
+		legendfontsize = 10,
 		guidefont = (16, :black),
 		tickfont = (12, :black),
 		minorticks = true,
 		legend = :bottomright,
 		# w = 2,
 		ylims = (0.0001, 0.1),
-		xlims = (0.0, 18)
+		xlims = (0.0, 17)
 	)
-	inconds = [(150, 20, 40, 2π/9, :ut), (180, 20, 40, 2π/9, :circ), (150, 20, 30, π/6, :hex), (180, 20, 30, π/6, :dt), (200, 20, 30, π/6, :rect), (150, 20, 20, π/9, :diamond), (180, 20, 20, π/9, :star5),  (200, 20, 20, π/9, :cross),   ]
+	inconds = [(150, 20, 40, 2π/9, :ut), (180, 20, 40, 2π/9, :circ), (150, 20, 30, π/6, :hex), (180, 20, 30, π/6, :dt), (200, 20, 30, π/6, :rect), (150, 20, 20, π/9, :diamond), (180, 20, 20, π/9, :star5),  (200, 20, 20, π/9, :rtriangle),  ]
 	for ics in inconds
 		iC = initial_data[(initial_data.R0 .== ics[1]) .& (initial_data.rr0 .== ics[2]) .& (initial_data.angle .== ics[3]), :]
 
-		LL = iC.realrr[1]# sqrt(iC.realrr[1] * iC.rr0[1]) #sqrt(iC.R0[1]*iC.maxh0[1])
 		ll = (iC.R0[1] + iC.realrr[1] - iC.rdrop[1])
-		om = ((0.01*0.0379*ics[4]^3/(sin(ics[4]))*sqrt(ics[4] - sin(2ics[4])/2)*1/LL))
 		tt = 3/2*(iC.R0[1] + iC.realrr[1] - iC.rdrop[1])/(0.01 * ics[4])
 		
 		tnorm = iC.tauMax[1] #(0.5 * 2iC.realrr / 0.01)
@@ -100,6 +98,21 @@ begin
 			# println("length: $(length(dataSamp.time))")
 		end
 	end
+	incondsRet = [(150, 40, 40, 2π/9, :circ), (150, 40, 30, π/6, :rect), (150, 40, 20, π/9, :ut),]
+	for ics in incondsRet
+		iC = initial_data[(initial_data.R0 .== ics[1]) .& (initial_data.rr0 .== ics[2]) .& (initial_data.angle .== ics[3]), :]
+
+		
+		ll = (iC.R0[1] + iC.realrr[1] - iC.rdrop[1])
+		tt = 3/2*(iC.R0[1] + iC.realrr[1] - iC.rdrop[1])/(0.01 * ics[4])
+		
+	 	dataSamp = growthDF[(growthDF.R0 .== ics[1]) .& (growthDF.rr0 .== ics[2]) .& (growthDF.theta .== ics[3]) .& (growthDF.substrate .== "uniform"), :]
+		scatter!(dataSamp.time[2:end] ./ tt, 
+		dataSamp.deltaH[2:end] ./ ll, 
+		label="(θ, ψ₀) = ($(ics[3])°, $(round(iC.psi0[1], digits=3)))",
+		m = (8, ics[5], 0.75, :white)
+		)
+	end
 	# println(tscale[1], finalH[1])
 	plot!(times_now ./ tscale[1], (0.003 .* exp.(0.5 .* times_now ./ tscale[1]) .+ 0.00) ./ finalH[1], label="Exponential fit", l = (2,  :dash, :black))
 	#plot!(subdata2.time[1:end] ./ T0, (0.001 .* exp.(0.000008 .* subdata2.time[1:end]) .+ 0.14) ./ H0, 
@@ -113,7 +126,45 @@ end
 savefig(growths, "../assets/growth-breakup.pdf")
 
 # ╔═╡ dd19e06e-999b-4a92-afc3-4020304f7bb2
-times_now
+begin
+	growthsDrop = plot(xlabel = L"t/\tau_m", ylabel = L"\Delta h / (R_0 - R_f)",
+		# xaxis=:log10,
+		yaxis=:log10,
+		grid = false,
+		legendfontsize = 11,
+		guidefont = (16, :black),
+		tickfont = (12, :black),
+		minorticks = true,
+		legend = :bottomright,
+		# w = 2,
+		ylims = (0.00001, 0.1),
+		xlims = (0.0, 60)
+	)
+	incondsDrop = [(200, 80, 20, π/9, :rt), (150, 80, 20, π/9, :ut), (180, 80, 20, π/9, :circ), (150, 40, 30, 2π/9, :hex), (180, 80, 40, 2π/9, :dt), (150, 40, 30, π/6, :rect), (180, 40, 30, π/6, :diamond)]
+	for ics in incondsDrop
+		iC = initial_data[(initial_data.R0 .== ics[1]) .& (initial_data.rr0 .== ics[2]) .& (initial_data.angle .== ics[3]), :]
+
+		LL = iC.realrr[1]# sqrt(iC.realrr[1] * iC.rr0[1]) #sqrt(iC.R0[1]*iC.maxh0[1])
+		ll = (iC.R0[1] + iC.realrr[1] - iC.rdrop[1])
+		om = ((0.01*0.0379*ics[4]^3/(sin(ics[4]))*sqrt(ics[4] - sin(2ics[4])/2)*1/LL))
+		tt = 3/2*(iC.R0[1] + iC.realrr[1] - iC.rdrop[1])/(0.01 * ics[4])
+		
+	 	dataSamp = growthDF[(growthDF.R0 .== ics[1]) .& (growthDF.rr0 .== ics[2]) .& (growthDF.theta .== ics[3]) .& (growthDF.substrate .== "uniform"), :]
+		scatter!(dataSamp.time[2:end] ./ tt, 
+		dataSamp.deltaH[2:end] ./ ll, 
+		label="(θ, ψ₀) = ($(ics[3])°, $(round(iC.psi0[1], digits=3)))",
+		m = (8, ics[5], 0.75, :white)
+		)
+		if ics[5] == :hex
+			push!(tscale, tt)
+			push!(finalH, iC.hdrop[1])
+			times_now .= dataSamp.time
+			# println("length: $(length(dataSamp.time))")
+		end
+	end
+	
+	growthsDrop
+end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
